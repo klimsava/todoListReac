03c653e1ed7c8ee2186451a2e8f217c409a28c89
Klim Savanovich <Klimsava96@mail.ru>
2019-10-29 17:25:59 +0300

    created events on click important, created click events executed

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,18 +2,47 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+    
+    state = {
+        done: false,
+        important: false
+    };
+
+    onLabelClick = () => {
+        this.setState({
+            done: true
+        });
+    };
+
+    onMarkImportant = () => {
+        this.setState({
+            important: true
+        });
+    }
+
     render() {
-        const { label, important} = this.props;
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold': 'normal'
+        const { label} = this.props;
+        const { done, important } = this.state;
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
+
+        if (important) {
+            classNames += ' important';
         }
         return (
-            <div className="todo-list-item">
-                <span style={style}>{label}</span>
+            <div className={classNames}>
+                <span 
+                    className="todo-list-item-label"
+                    onClick={this.onLabelClick}>{label}</span>
 
                 <div className="buttons">
-                    <button type="button" className="btn btn-outline-success btn-sm">
+                    <button 
+                        type="button" 
+                        className="btn btn-outline-success btn-sm"
+                        onClick = {this.onMarkImportant}>
                         <i className="fa fa-exclamation" />
                     </button>
 
